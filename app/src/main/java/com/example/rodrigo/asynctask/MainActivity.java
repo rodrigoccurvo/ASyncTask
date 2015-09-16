4cac422a2385f317c74e13a742bf38adc16204a7
Rodrigo Coin Curvo <rodrigoccurvo@gmail.com>
2015-09-16 13:00:46 -0400

    Tarefa 12 completa

--- app/src/main/java/com/example/rodrigo/asynctask/MainActivity.java
+++ app/src/main/java/com/example/rodrigo/asynctask/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.rodrigo.asynctask;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -8,12 +12,20 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.rodrigo.asynctask.data.ContratoDB;
+import com.example.rodrigo.asynctask.data.NoticiaDBHelper;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private ArrayAdapter<String> mAdapter;
+    private Map<Integer, Long> mapaIds = new HashMap<Integer, Long>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +47,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        atualizar();
+        atualizaInterface();
     }
 
     @Override
@@ -70,22 +82,74 @@ public class MainActivity extends ActionBarActivity {
         pega.execute();
     }
 
-    public class PegaDadosDoServidor extends AsyncTask<Void, Void, String[]> {
+    private void atualizaInterface() {
+        SQLiteOpenHelper dbHelper = new NoticiaDBHelper(getApplicationContext());
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        Cursor cursor = db.query(
+                ContratoDB.Noticia.NOME_TABELA, // Tabela
+                null, // colunas (todas)
+                null, // colunas para o where
+                null, // valores para o where
+                null, // group by
+                null, // having
+                null  // sort by
+        );
+
+        mAdapter.clear();
+        mapaIds.clear();
+
+        while (cursor.moveToNext()) {
+            long id = cursor.getLong(cursor.getColumnIndex(ContratoDB.Noticia._ID));
+            long data = cursor.getLong(cursor.getColumnIndex(ContratoDB.Noticia.COLUNA_DATA));
+            String titulo = cursor.getString(cursor.getColumnIndex(ContratoDB.Noticia.COLUNA_TITULO));
+            String texto = cursor.getString(cursor.getColumnIndex(ContratoDB.Noticia.COLUNA_TEXTO));
+
+            String dataBonita = new SimpleDateFormat("dd/MM/yyyy").format(new Date(data * 1000));
+
+            // Associa a posição do item ao id dele
+            mapaIds.put(mAdapter.getCount(), id);
+
+            // Não estou usando o texto, mas poderia
+            mAdapter.add(id + " - " + dataBonita + " - " + titulo);
+        }
+
+        db.close();
+        dbHelper.close();
+    }
+
+    public class PegaDadosDoServidor extends AsyncTask<Void, Void, String[][]> {
 
         @Override
-        protected String[] doInBackground(Void... voids) {
+        protected String[][] doInBackground(Void... voids) {
             ServidorFalso servidor = new ServidorFalso();
             return servidor.pegaDados();
         }
 
         @Override
-        protected void onPostExecute(String[] result) {
+        protected void onPostExecute(String[][] result) {
             if (result != null) {
-                mAdapter.clear();
-                for(String r : result) {
-                    mAdapter.add(r);
+                SQLiteOpenHelper dbHelper = new NoticiaDBHelper(getApplicationContext());
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+                // LIMPA A TABELA INTEIRA!!!!
+                db.delete(ContratoDB.Noticia.NOME_TABELA, null, null);
+
+                for(String linha[] : result) {
+                    ContentValues entrada = new ContentValues();
+                    entrada.put(ContratoDB.Noticia.COLUNA_DATA, linha[0]);
+                    entrada.put(ContratoDB.Noticia.COLUNA_TITULO, linha[1]);
+                    entrada.put(ContratoDB.Noticia.COLUNA_TEXTO, linha[2]);
+
+                    db.insert(ContratoDB.Noticia.NOME_TABELA, null, entrada);
+
                 }
+
+                db.close();
+                dbHelper.close();
             }
+
+            atualizaInterface();
         }
     }
 }
--- app/src/main/java/com/example/rodrigo/asynctask/ServidorFalso.java
+++ app/src/main/java/com/example/rodrigo/asynctask/ServidorFalso.java
@@ -4,17 +4,16 @@ package com.example.rodrigo.asynctask;
  * Created by rodrigo on 9/2/15.
  */
 public class ServidorFalso {
-    public String[] pegaDados() {
-        String[] dados1 = {
-                "Um",
-                "Dois",
-                "Três indiozinhos"
+    public String[][] pegaDados() {
+        String[][] dados1 = {
+                {"1442361600", "Vovó campeã de kickbox", "Uma avozinha ganhou o campeonato de kickbox"},
+                {"1442448000", "Uma notícia", "Um texto bem interessante"},
+                {"1442534400", "Outra notícia", "Outro texto não tão interessante"}
         };
 
-        String[] dados2 = {
-                "Quatro",
-                "Cinco",
-                "Seis indiozinhos"
+        String[][] dados2 = {
+                {"1442620800", "Mais uma notícia", "Mais um texto"},
+                {"1442707200", "Última notícia", "Último texto"}
         };
 
         // Para não pegar sempre o mesmo...
